a30a7a2aeb65672995682c4934f45f087bbece8c
shamilbi <shamilbi@gmail.com>
2020-10-17 13:47:19 +0700

    challenge #1 redisolar/dao/redis/site.py          def find_all(self, **kwargs) -> Set[Site]:              site_hashes = []  # type: ignore     +        for site_id in self.redis.sscan_iter(self.key_schema.site_ids_key()):     +            hash_key = self.key_schema.site_hash_key(site_id)     +            site_hash = self.redis.hgetall(hash_key)     +            site_hashes.append(site_hash) tests/dao/redis/test_site.py     -@pytest.mark.skip("Remove for challenge #1")     +#@pytest.mark.skip("Remove for challenge #1")      def test_find_all(site_dao):

--- redisolar/dao/redis/site.py
+++ redisolar/dao/redis/site.py
@@ -37,8 +37,11 @@ class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
     def find_all(self, **kwargs) -> Set[Site]:
         """Find all Sites in Redis."""
         # START Challenge #1
-        # Remove this line when you've written code to build `site_hashes`.
         site_hashes = []  # type: ignore
+        for site_id in self.redis.sscan_iter(self.key_schema.site_ids_key()):
+            hash_key = self.key_schema.site_hash_key(site_id)
+            site_hash = self.redis.hgetall(hash_key)
+            site_hashes.append(site_hash)
         # END Challenge #1
 
         return {FlatSiteSchema().load(site_hash) for site_hash in site_hashes}
